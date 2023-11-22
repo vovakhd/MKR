beb37b623f456483e4c9fda8deb3737e3755b964
vovakhd <146945222+vovakhd@users.noreply.github.com>
2023-11-22 18:03:19 +0200

    Пустий проект

---
+++ project_oop/src/test/java/oop/example/project_oop/ProjectOopApplicationTests.java
@@ -0,0 +1,13 @@
+package oop.example.project_oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
