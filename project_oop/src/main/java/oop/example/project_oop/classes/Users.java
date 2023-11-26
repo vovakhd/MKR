8b4866de53c81b8241c630485a626be0846c1b62
cheliuskina04 <122870686+cheliuskina04@users.noreply.github.com>
2023-11-26 16:49:25 +0200

    vocabulary csv

--- project_oop/src/main/java/oop/example/project_oop/classes/Users.java
+++ project_oop/src/main/java/oop/example/project_oop/classes/Users.java
@@ -1,5 +1,15 @@
 package oop.example.project_oop.classes;
 
+import org.supercsv.io.CsvMapReader;
+import org.supercsv.io.CsvMapWriter;
+import org.supercsv.io.ICsvMapReader;
+import org.supercsv.io.ICsvMapWriter;
+import org.supercsv.prefs.CsvPreference;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
 public class Users {
 
     private int id;
@@ -28,4 +38,36 @@ public class Users {
         return "User{" + "id='" + id + ", username='"+ ", email='" + email + '\'' + '}';
     }
 
+    public static void addUserToVocabulary(String header_name) throws IOException {
+        //add new column for new user in the vocabulary
+        ICsvMapReader Reader = null;
+        ICsvMapWriter Writer = null;
+        try {
+            //read vocabulary's headers, add new column
+            Reader = new CsvMapReader(new FileReader("vocabulary.csv"), CsvPreference.STANDARD_PREFERENCE);
+            final String[] oldHeaders = Reader.getHeader(true);
+            final String[] newHeaders = new String[oldHeaders.length + 1];
+            System.arraycopy(oldHeaders, 0, newHeaders, 0, oldHeaders.length);
+            newHeaders[newHeaders.length - 1] = header_name;
+
+            //write newHeaders with new column
+            Writer = new CsvMapWriter(new FileWriter("vocabulary.csv"), CsvPreference.STANDARD_PREFERENCE);
+            Writer.writeHeader(newHeaders);
+            //fill column with -1 for every word
+            Map<String, String> row;
+            while ((row = Reader.read(oldHeaders)) != null) {
+                row.put(header_name, "-1");
+                Writer.write(row, newHeaders);
+            }
+
+        } finally { //close file
+            if (Reader != null) {
+                Reader.close();
+            }
+            if (Writer != null) {
+                Writer.close();
+            }
+        }
+
+    }
 }
---
+++ project_oop/src/main/java/oop/example/project_oop/classes/Word.java
@@ -0,0 +1,25 @@
+package oop.example.project_oop.classes;
+
+import lombok.Getter;
+import org.supercsv.io.CsvMapReader;
+import org.supercsv.io.ICsvMapReader;
+import org.supercsv.prefs.CsvPreference;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+
+@Getter
+public final class Word {
+    final private String word;
+    final private String translate;
+    private int num;
+
+
+    public Word() {
+        this.word = "word";
+        this.translate = "translation";
+        this.num = 0;
+    }
+}
