97f49fbd6fb6d1139e98f6098d2bc4b71a5d0065
Shimko Vadim <simkovadim887@gmail.com>
2023-11-26 19:43:22 +1400

    функції в Data

---
+++ project_oop/src/main/java/oop/example/project_oop/Data/WordData.java
@@ -0,0 +1,14 @@
+package oop.example.project_oop.Data;
+
+import org.supercsv.io.CsvMapReader;
+import org.supercsv.io.ICsvMapReader;
+import org.supercsv.prefs.CsvPreference;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+
+public class WordData {
+
+}
