357d6d4f93746d7fbe399b7b9336ba3545316c64
Vova <vovakhodakivskyy1220@gmail.com>
2023-11-27 19:55:46 +0200

    update class Lessons

--- project_oop/src/main/java/oop/example/project_oop/classes/Lessons.java
+++ project_oop/src/main/java/oop/example/project_oop/classes/Lessons.java
@@ -1,11 +1,101 @@
 package oop.example.project_oop.Classes;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import lombok.Getter;
+import lombok.Setter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.text.DecimalFormat;
 
-//@Data
-//@AllArgsConstructor
-//public class Lessons extends Levels {
-//    private String levelNumber;
-//    private String levelContent;
-//}
+
+@Setter
+@Getter
+public class Lessons {
+    private String numberOfLesson;
+    private String lessonProgress;
+    public double calculateProgress(String userColumn, String targetLevel, int targetLesson) throws IOException, CsvException {
+        List<String[]> words = readCSV();
+
+        // Лічильники для вивчених та всіх слів
+        int learnedWordsCount = 0;
+        int totalWordsCount = 0;
+
+        // Знаходження індексу колонки користувача в рядках CSV
+        int userColumnIndex = -1;
+        String[] header = words.get(0); // Перший рядок містить заголовки колонок
+        for (int i = 0; i < header.length; i++) {
+            if (header[i].equals(userColumn)) {
+                userColumnIndex = i;
+                break;
+            }
+        }
+
+        if (userColumnIndex == -1) {
+            throw new CsvException("User column not found in CSV.");
+        }
+
+        for (String[] word : words) {
+            String level = word[0];
+            int lesson;
+            int userIndicator;
+
+            try {
+                lesson = Integer.parseInt(word[1]);
+                userIndicator = Integer.parseInt(word[userColumnIndex]);
+            } catch (NumberFormatException e) {
+                continue;
+            }
+
+            if (level.equals(targetLevel) && lesson == targetLesson) {
+                totalWordsCount++;
+
+                if (userIndicator >= 5) {
+                    learnedWordsCount++;
+                }
+            }
+        }
+
+        // Розрахунок прогресу у відсотках
+        if (totalWordsCount > 0) {
+            double progressPercentage = (double) learnedWordsCount / totalWordsCount * 100;
+            // Округлення до 2 знаків після коми
+            DecimalFormat decimalFormat = new DecimalFormat("#.##");
+            String formattedProgress = decimalFormat.format(progressPercentage);
+
+            // Заміна коми на крапку (для запобігання NumberFormatException)
+            formattedProgress = formattedProgress.replace(',', '.');
+            return Double.parseDouble(formattedProgress);
+        } else {
+            return 0.0;  // Якщо немає слів, прогрес дорівнює 0
+        }
+    }
+
+    // Функція для зчитування даних з CSV-файлу
+    private List<String[]> readCSV() throws IOException, CsvException {
+        String file = "vocabulary.csv";
+        List<String[]> words = new ArrayList<>();
+        try (CSVReader csvReader = new CSVReaderBuilder(new FileReader(file)).build()) {
+            String[] headers = csvReader.readNext();
+            if (headers == null) {
+                throw new CsvException("CSV file is empty or does not contain headers.");
+            }
+            words.add(headers);
+            words.addAll(csvReader.readAll());
+        }
+        return words;
+    }
+    public static void main(String[] args) {
+        Lessons lessons = new Lessons();
+        try {
+            double progress = lessons.calculateProgress("NEWuser", "A", 2);
+            System.out.println("Progress: " + progress + "%");
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
