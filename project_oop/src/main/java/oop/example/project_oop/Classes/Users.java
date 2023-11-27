dc1a7a032412a89c886fd141631ffe4a208f5e2c
cheliuskina04 <122870686+cheliuskina04@users.noreply.github.com>
2023-11-27 14:45:21 +0200

    Squashed commit of the following:
    commit cea2007427c7eb6ca9eb41626232c3c868fb0f2e
    Author: cheliuskina04 <122870686+cheliuskina04@users.noreply.github.com>
    Date:   Mon Nov 27 14:32:22 2023 +0200
    
        UserController
    
    commit f900b19fe8ee48c97711e784900a39a27a72c946
    Author: cheliuskina04 <122870686+cheliuskina04@users.noreply.github.com>
    Date:   Sun Nov 26 19:33:09 2023 +0200
    
        security working pages
    
    commit 00442d23874d623cfe6eae83a5354fea3fe8e8de
    Author: cheliuskina04 <122870686+cheliuskina04@users.noreply.github.com>
    Date:   Sun Nov 26 17:31:39 2023 +0200
    
        security pages

---
+++ project_oop/src/main/java/oop/example/project_oop/Classes/Users.java
@@ -0,0 +1,38 @@
+package oop.example.project_oop.Classes;
+
+import java.util.Objects;
+
+public class Users {
+
+    private int id;
+    private String email;
+    private String password;
+
+    public Users(int id,String email,String password){
+        this.id=id;
+        this.email=email;
+        this.password=password;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public String getEmail(){
+        return  email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void changePassword(String newPassword){
+        this.password=newPassword;
+    }
+    public String information(){
+        return "User{" + "id='" + id + ", username='"+ ", email='" + email + '\'' + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        this.id=id;
+        return Objects.hash(email);
+    }
+}
---
+++ project_oop/src/main/java/oop/example/project_oop/Classes/Word.java
@@ -0,0 +1,27 @@
+package oop.example.project_oop.Classes;
+
+import lombok.Getter;
+
+@Getter
+public final class Word {
+    final private String word;
+    final private String translate;
+    private int num;
+
+
+    public Word() {
+        this.word = "word";
+        this.translate = "translation";
+        this.num = 0;
+    }
+    public String getWord(){
+        return word;
+    }
+    public String getTranslate(){
+        return  translate;
+    }
+
+    public int getNum(){
+        return num;
+    }
+}
