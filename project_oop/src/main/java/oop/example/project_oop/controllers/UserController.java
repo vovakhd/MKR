aa6e3dd96822e5f93fd2c79b69be8b9192651f20
cheliuskina04 <122870686+cheliuskina04@users.noreply.github.com>
2023-11-28 13:49:52 +0200

    SecurityConfig & UserController

---
+++ project_oop/src/main/java/oop/example/project_oop/controllers/UserController.java
@@ -0,0 +1,29 @@
+package oop.example.project_oop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class UserController {
+    @GetMapping("/")
+    public String welcome1(){
+        return "welcome";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome";
+    }
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
