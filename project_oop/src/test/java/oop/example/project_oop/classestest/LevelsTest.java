20b9e2f02e610bb5a060a8e8a281dc85250526c6
Vova <vovakhodakivskyy1220@gmail.com>
2023-12-03 12:41:44 +0200

    update LevelsTest
    update LevelsTest
    
    Merge branch 'word-entity' of https://github.com/vovakhd/MKR into word-entity

--- project_oop/src/test/java/oop/example/project_oop/classestest/LevelsTest.java
+++ project_oop/src/test/java/oop/example/project_oop/classestest/LevelsTest.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import oop.example.project_oop.classes.Levels;
 
+
 public class LevelsTest {
 
     @Test
     public void testCalculateLevelProgress() {
-        double levelProgress = Levels.calculateLevelProgress("user@mail", "B");
-        assertEquals(48.15, levelProgress, 0.01);
+        double levelProgress = Levels.calculateLevelProgress("user@mail", "A");
+        assertEquals(29.03, levelProgress, 0.01);
     }
 
     @Test
