359ee8e6a79450a4567bce048af75a3eece85c7c
cheliuskina04 <122870686+cheliuskina04@users.noreply.github.com>
2023-12-03 11:54:04 +0200

    fixed tests

--- project_oop/src/test/java/oop/example/project_oop/classestest/WordTest.java
+++ project_oop/src/test/java/oop/example/project_oop/classestest/WordTest.java
@@ -4,22 +4,21 @@ import oop.example.project_oop.classes.Word;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class WordTest {
+public class WordTest{
+        @Test
+        public void testWordClass() {
+            Word word = new Word("apple", "яблуко", 3, 1L);
 
-    @Test
-    public void testWordClass() {
-        Word word = new Word("apple", "яблуко", 3);
+            assertEquals("apple", word.getWord());
+            assertEquals("яблуко", word.getTranslate());
+            assertEquals(3, word.getIndicator());
+        }
 
-        assertEquals("apple", word.getWord());
-        assertEquals("яблуко", word.getTranslate());
-        assertEquals(3, word.getIndicator());
-    }
+        @Test
+        public void testSetIndicator() {
+            Word word = new Word("apple", "яблуко", 3,1L);
+            word.setIndicator(5);
 
-    @Test
-    public void testSetIndicator() {
-        Word word = new Word("apple", "яблуко", 3);
-        word.setIndicator(5);
-
-        assertEquals(5, word.getIndicator());
-    }
+            assertEquals(5, word.getIndicator());
+        }
 }
